baf002c9cf9958ef4e153e768e32e47e6e163a64
DungNK <dungnkse04821@gmail.com>
2025-09-24 23:44:04 +0700

    product grid view

---
+++ src/ShopManagement.Web/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.EntityDto;
+using ShopManagement.IShopManagementService;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Web.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IProductAppService _productAppService;
+
+        public List<ProductDto> Products { get; set; } = new();
+
+        public IndexModel(IProductAppService productAppService)
+        {
+            _productAppService = productAppService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            // Lấy danh sách sản phẩm từ service
+            Products = (await _productAppService.GetListAsync()).ToList();
+        }
+    }
+}
