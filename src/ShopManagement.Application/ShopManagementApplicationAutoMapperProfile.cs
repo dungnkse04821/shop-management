714dfb345f2f59a66a5303ed65b2c49ea4796fc1
DungNK <dungnkse04821@gmail.com>
2025-10-27 23:58:29 +0700

    add automapper

--- src/ShopManagement.Application/ShopManagementApplicationAutoMapperProfile.cs
+++ src/ShopManagement.Application/ShopManagementApplicationAutoMapperProfile.cs
@@ -13,10 +13,6 @@ public class ShopManagementApplicationAutoMapperProfile : Profile
 
         CreateMap<ProductVariant, ProductVariantDto>();
 
-        // Nếu có Create/Update DTO thì map ngược lại
-        //CreateMap<CreateUpdateProductDto, Product>();
-        //CreateMap<CreateUpdateProductVariantDto, ProductVariant>();
-
         CreateMap<CreateUpdateProductDto, Product>()
         .ForMember(dest => dest.Variants, opt => opt.MapFrom(src => src.Variants));
 
@@ -33,5 +29,7 @@ public class ShopManagementApplicationAutoMapperProfile : Profile
 
         CreateMap<ProductImage, ProductImageDto>();
         CreateMap<ProductImageDto, ProductImage>();
+
+        CreateMap<CreateUpdateProductImageDto,ProductImage>().ReverseMap();
     }
 }
